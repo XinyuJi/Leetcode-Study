7a0920515227a5caef56adc1b0a4be5c727b082c
XinyuJi <56640682+XinyuJi@users.noreply.github.com>
2020-01-27 22:55:21 +0100

    Create 35_Search_Insert_Position.py
    think myself! :+1:

---
+++ Problems/Python/35_Search_Insert_Position.py
@@ -0,0 +1,34 @@
+'''
+Runtime: 48 ms, faster than 74.24% of Python3 online submissions for Search Insert Position.
+Memory Usage: 13.4 MB, less than 100.00% of Python3 online submissions for Search Insert Position.
+'''
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
+
+
+'''
+Testing Code
+
+list1 = [1,3,5,6]
+val = 6
+list1 = [1,3,5,6]
+val = 2
+list1 = [1,3,5,6]
+val = 7
+list1 = [1,3,5,6]
+val = 0
+
+if val in list1:
+    print (list1.index(val))
+else:
+    list1.append(val)
+    list1.sort()
+    print(list1.index(val))
+‘’‘
