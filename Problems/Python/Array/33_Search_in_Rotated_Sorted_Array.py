c9e6eb1badf811fb1cd68be5244ae669118106c1
XinyuJi <56640682+XinyuJi@users.noreply.github.com>
2020-02-12 15:58:47 +0100

    Update 33_Search_in_Rotated_Sorted_Array.py

--- Problems/Python/Array/33_Search_in_Rotated_Sorted_Array.py
+++ Problems/Python/Array/33_Search_in_Rotated_Sorted_Array.py
@@ -14,3 +14,21 @@ class Solution(object):
             if nums[i] == target:
                 return i
         return -1
+
+
+"""
+Runtime: 28 ms, faster than 70.17% of Python online submissions for Search in Rotated Sorted Array.
+Memory Usage: 12.2 MB, less than 6.12% of Python online submissions for Search in Rotated Sorted Array.
+"""
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
