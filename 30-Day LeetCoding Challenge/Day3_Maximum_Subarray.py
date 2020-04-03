e5496ed63dea0ae50653b31d8bbfd9b320ffa50c
XinyuJi <56640682+XinyuJi@users.noreply.github.com>
2020-04-03 15:33:53 +0200

    Create Day3_Maximum_Subarray.py

---
+++ 30-Day LeetCoding Challenge/Day3_Maximum_Subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if max(nums) <= 0:
+            return max(nums)
+        local_max = 0
+        global_max = 0
+        for num in nums:
+            local_max = max(0, num + local_max)
+            global_max = max (global_max, local_max)
+        return global_max
