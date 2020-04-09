5f6c00c714272cf31e0d35a0ee64e4f207ac2d84
XinyuJi <56640682+XinyuJi@users.noreply.github.com>
2020-04-10 01:34:39 +0200

    Create Day9_Backspace_String_Compare.py

---
+++ 30-Day LeetCoding Challenge/Day9_Backspace_String_Compare.py
@@ -0,0 +1,18 @@
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        s = ''
+        t = ''
+
+        for i in S:
+            if i == "#":
+                s = s[:-1]
+            else:
+                s = s + i
+
+        for j in T:
+            if j == "#":
+                t = t[:-1]
+            else:
+                t = t + j
+
+        return s == t
