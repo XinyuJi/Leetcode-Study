8fafa42cf7189e537bda5ed17c6c4336827b6c8d
XinyuJi <56640682+XinyuJi@users.noreply.github.com>
2020-04-12 17:17:46 +0200

    Create Day12_Last_Stone_Weight.py

---
+++ 30-Day LeetCoding Challenge/Day12_Last_Stone_Weight.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        if len(stones) == 1:
+            return "".join(str(x) for x in stones)
+
+        while(len(stones) > 1):
+            x = sorted(stones)[len(stones)-1]
+            y = sorted(stones)[len(stones)-2]
+            if x == y:
+                stones.remove(x)
+                stones.remove(y)
+            else:
+                stones.append(x-y)
+                stones.remove(x)
+                stones.remove(y)
+
+        if len(stones) == 1:
+            return "".join(str(x) for x in stones)
+        else:
+            return 0
