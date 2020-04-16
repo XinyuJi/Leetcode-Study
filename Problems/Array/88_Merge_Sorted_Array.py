2245fbb110c0023c7a1491b93480d549b7cb58b2
XinyuJi <56640682+XinyuJi@users.noreply.github.com>
2020-04-17 00:00:50 +0200

    Update 88_Merge_Sorted_Array.py

--- Problems/Array/88_Merge_Sorted_Array.py
+++ Problems/Array/88_Merge_Sorted_Array.py
@@ -14,3 +14,28 @@ class Solution:
             nums2.remove(nums2[len(nums2)-1])
         nums1.extend(nums2)
         nums1.sort()
+
+
+"""
+[2nd time] After 3 months
+Runtime: 28 ms, faster than 96.76% of Python3 online submissions for Merge Sorted Array.
+Memory Usage: 14 MB, less than 6.15% of Python3 online submissions for Merge Sorted Array.
+"""
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if m < len(nums1):
+            for i in range(len(nums1)-1, m-1, -1):
+                del nums1[i]
+
+        if n < len(nums2):
+            for j in range(len(nums2)-1, n-1, -1):
+                del nums2[j]
+
+        for i in range(n):
+            nums1.append(nums2[i])
+
+        nums1.sort()
