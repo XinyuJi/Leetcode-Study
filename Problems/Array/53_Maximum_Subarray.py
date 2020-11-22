e66a7027109ddb656e14496d82080581d1556899
XinyuJi <56640682+XinyuJi@users.noreply.github.com>
2020-11-22 20:48:15 +0100

    preparing

--- Problems/Array/53_Maximum_Subarray.py
+++ Problems/Array/53_Maximum_Subarray.py
@@ -13,3 +13,30 @@ class Solution:
             local_max = max(0, num + local_max)
             global_max = max (global_max, local_max)
         return global_max
+
+#####2020.11.22#####
+"""
+Runtime: 40 ms, faster than 97.24% of Python online submissions for Maximum Subarray.
+Memory Usage: 14.4 MB, less than 34.83% of Python online submissions for Maximum Subarray.
+"""
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1:
+            return nums[0]
+        
+        current = nums[0]
+        MaxSum = nums[0]
+        
+        for num in nums[1:]:
+            if (num + current > current) and (num + current > num):
+                current += num
+            else:
+                MaxSum = max(current, MaxSum)
+                current = max(num, current+num)
+
+        return max(MaxSum, current)
--- waitinglist.md
+++
@@ -1,4 +0,0 @@
-1004. Max Consecutive Ones III
-152. Maximum Product Subarray
-
-Remove Duplicates from Sorted Array 系列
