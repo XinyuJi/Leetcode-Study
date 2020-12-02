58019cb88015cb96b97bc2a662a35e296e192aaa
XinyuJi <56640682+XinyuJi@users.noreply.github.com>
2020-12-02 09:16:34 +0100

    Create 141_Linked_List_Cycle.py

---
+++ Problems/Linked List/141_Linked_List_Cycle.py
@@ -0,0 +1,24 @@
+"""
+Runtime: 40 ms, faster than 64.59% of Python online submissions for Linked List Cycle.
+Memory Usage: 19.9 MB, less than 31.78% of Python online submissions for Linked List Cycle.
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
